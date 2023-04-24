c762126dfe22c7365d801f1a5fdd8bdccf32f919
Jonathan <jonathanfelipe.muller@gmail.com>
2023-04-24 20:00:33 -0300

    app-service-function

---
+++ index.js
@@ -0,0 +1,18 @@
+exports = function (changeEvent) {
+  const movies = context.services
+    .get("mongodb-atlas")
+    .db("sample_mflix")
+    .collection("movies");
+
+  const documentId = changeEvent.documentKey._id;
+  const magicField = { magic_just_happened: true };
+
+  movies
+    .updateOne({ _id: documentId }, { $set: magicField })
+    .then((result) => {
+      console.log("Document updated successfully:", result);
+    })
+    .catch((error) => {
+      console.log("Error updating document:", error);
+    });
+};
